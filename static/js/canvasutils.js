bc80f4042bb4424e6c2204fbfb5c2b57c4fc4c41
MINH KHANG TRAN <ktran2@oswego.edu>
2022-04-19 17:03:39 -0400

    Add Image Upload function

--- static/js/canvasutils.js
+++ static/js/canvasutils.js
@@ -74,6 +74,21 @@ function handleForm(event)
     let birthDate = memberForm.elements[FORM_INPUT_LABELS.BIRTHDATE].value;
     let note = memberForm.elements[FORM_INPUT_LABELS.NOTE].value;
 
+    const form_data = new FormData($('#memberForm')[0]);
+    $.ajax({
+        type: 'POST',
+        url: `${window.origin}/saveImage/${treeID}`,
+        data: form_data,
+        contentType: false,
+        cache: false,
+        processData: false,
+        success: function(response) {
+            if (response['message'] === "OK") {
+                console.log(response['image_path']);
+            }
+        },
+    });
+
     if(text === FORM_HEADERS.EDIT_MEMBER)
     {
         if(memberSelected !== -1)
