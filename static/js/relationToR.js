bdba67e678b72a1fb4df0e38e729614297eee635
joemontalvo <98854432+joemontalvo@users.noreply.github.com>
2022-04-29 09:53:36 -0400

    fixed dotted relations not saving
    also removed the help button JQuery stuff because the help button does not exist anymore and there was an error every time you opened the edit page because of it.
    
    Changed the relations so that they have a mode 'h' or 'v' which allows them to be draw horizontally or vertically. and there is now a option in the sidebar to toggle that instead of using ' , '

--- static/js/relationToR.js
+++ static/js/relationToR.js
@@ -2,7 +2,7 @@ class RelationToR
 {
     TYPE = RELATION_TYPE.M_TO_R;
     selected = false;
-    modeModifier = 1;
+    mode = "v";
     dotted = false;
 
     centerX = 0;
@@ -34,12 +34,6 @@ class RelationToR
         const My = members[members.findIndex(object => { return object.id === this.MemID;})].y;
         const Rx = relations[relations.findIndex(object => { return object.id === this.RelID;})].centerX;
         const Ry = relations[relations.findIndex(object => { return object.id === this.RelID;})].centerY;
-        this.mode = 0;
-
-        if(Mx !== Rx && My !== Ry)
-        {
-            this.mode = this.modeModifier % 2 + 1;
-        }
 
         this.centerX = (Mx + Rx) / 2;
         this.centerY = (My + Ry) / 2;
@@ -47,35 +41,53 @@ class RelationToR
         switch(this.mode)
         {
             //Straight line.
-            case 0:
-                if(this.selected) line(canvas, Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)), Rx + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), (gridSize / 2) + (BorderSize * 2), this.dotted, COLOR_PALETTE.SELECTED);
-                line(canvas, Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)), Rx + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), (gridSize / 2), (gridSize / 2), this.dotted);
-                break;
+            // case 0:
+            //     if(this.selected) line(canvas, Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)), Rx + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), (gridSize / 2) + (BorderSize * 2), this.dotted, COLOR_PALETTE.SELECTED);
+            //     line(canvas, Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)), Rx + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), (gridSize / 2), (gridSize / 2), this.dotted);
+            //     break;
             //Segmented line - vertical junction.
-            case 1:
-                const distX = (Rx - Mx) * this.junction;
-                if(this.selected)
+            case "v":
+
+                if(Mx === Rx || My === Ry)
                 {
-                    line(canvas, Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)), Mx + distX + gridSize, My + (gridSize * 2 - (gridSize / 2)), (gridSize / 2) + (BorderSize * 2), this.dotted, COLOR_PALETTE.SELECTED);
-                    line(canvas, Mx + distX + gridSize, My + (gridSize * 2 - (gridSize / 2)), Mx + distX + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), (gridSize / 2) + (BorderSize * 2), this.dotted, COLOR_PALETTE.SELECTED);
-                    line(canvas, Mx + distX + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), Rx + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), (gridSize / 2) + (BorderSize * 2), this.dotted, COLOR_PALETTE.SELECTED);
+                    if(this.selected) line(canvas, Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)), Rx + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), (gridSize / 2) + (BorderSize * 2), this.dotted, COLOR_PALETTE.SELECTED);
+                    line(canvas, Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)), Rx + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), (gridSize / 2), (gridSize / 2), this.dotted);
                 }
-                line(canvas, Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)), Mx + distX + gridSize, My + (gridSize * 2 - (gridSize / 2)), (gridSize / 2), this.dotted);
-                line(canvas, Mx + distX + gridSize, My + (gridSize * 2 - (gridSize / 2)), Mx + distX + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), (gridSize / 2), this.dotted);
-                line(canvas, Mx + distX + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), Rx + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), (gridSize / 2), this.dotted);
+                else
+                {
+                   const distX = (Rx - Mx) * this.junction;
+                    if(this.selected)
+                    {
+                        line(canvas, Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)), Mx + distX + gridSize, My + (gridSize * 2 - (gridSize / 2)), (gridSize / 2) + (BorderSize * 2), this.dotted, COLOR_PALETTE.SELECTED);
+                        line(canvas, Mx + distX + gridSize, My + (gridSize * 2 - (gridSize / 2)), Mx + distX + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), (gridSize / 2) + (BorderSize * 2), this.dotted, COLOR_PALETTE.SELECTED);
+                        line(canvas, Mx + distX + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), Rx + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), (gridSize / 2) + (BorderSize * 2), this.dotted, COLOR_PALETTE.SELECTED);
+                    }
+                    line(canvas, Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)), Mx + distX + gridSize, My + (gridSize * 2 - (gridSize / 2)), (gridSize / 2), this.dotted);
+                    line(canvas, Mx + distX + gridSize, My + (gridSize * 2 - (gridSize / 2)), Mx + distX + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), (gridSize / 2), this.dotted);
+                    line(canvas, Mx + distX + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), Rx + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), (gridSize / 2), this.dotted);
+                }
                 break;
             //Segmented line - horizontal junction.
-            case 2:
-                const distY = (Ry - My) * this.junction;
-                if(this.selected)
+            case "h":
+
+                if(Mx === Rx || My === Ry)
                 {
-                    line(canvas, Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)), Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)) + distY, (gridSize / 2) + (BorderSize * 2), this.dotted, COLOR_PALETTE.SELECTED);
-                    line(canvas, Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)) + distY, Rx + gridSize, My + (gridSize * 2 - (gridSize / 2)) + distY, (gridSize / 2) + (BorderSize * 2), this.dotted, COLOR_PALETTE.SELECTED);
-                    line(canvas, Rx + gridSize, My + (gridSize * 2 - (gridSize / 2)) + distY, Rx + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), (gridSize / 2) + (BorderSize * 2), this.dotted, COLOR_PALETTE.SELECTED);
+                    if(this.selected) line(canvas, Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)), Rx + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), (gridSize / 2) + (BorderSize * 2), this.dotted, COLOR_PALETTE.SELECTED);
+                    line(canvas, Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)), Rx + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), (gridSize / 2), (gridSize / 2), this.dotted);
                 }
-                line(canvas, Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)), Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)) + distY, (gridSize / 2), this.dotted);
-                line(canvas, Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)) + distY, Rx + gridSize, My + (gridSize * 2 - (gridSize / 2)) + distY, (gridSize / 2), this.dotted);
-                line(canvas, Rx + gridSize, My + (gridSize * 2 - (gridSize / 2)) + distY, Rx + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), (gridSize / 2), this.dotted);
+                else
+                {
+                    const distY = (Ry - My) * this.junction;
+                    if(this.selected)
+                    {
+                        line(canvas, Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)), Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)) + distY, (gridSize / 2) + (BorderSize * 2), this.dotted, COLOR_PALETTE.SELECTED);
+                        line(canvas, Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)) + distY, Rx + gridSize, My + (gridSize * 2 - (gridSize / 2)) + distY, (gridSize / 2) + (BorderSize * 2), this.dotted, COLOR_PALETTE.SELECTED);
+                        line(canvas, Rx + gridSize, My + (gridSize * 2 - (gridSize / 2)) + distY, Rx + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), (gridSize / 2) + (BorderSize * 2), this.dotted, COLOR_PALETTE.SELECTED);
+                    }
+                    line(canvas, Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)), Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)) + distY, (gridSize / 2), this.dotted);
+                    line(canvas, Mx + gridSize, My + (gridSize * 2 - (gridSize / 2)) + distY, Rx + gridSize, My + (gridSize * 2 - (gridSize / 2)) + distY, (gridSize / 2), this.dotted);
+                    line(canvas, Rx + gridSize, My + (gridSize * 2 - (gridSize / 2)) + distY, Rx + gridSize, Ry + (gridSize * 2 - (gridSize / 2)), (gridSize / 2), this.dotted);
+                }
                 break;
         }
     }
@@ -100,17 +112,17 @@ class RelationToR
 
         switch(this.mode)
         {
-            case 0: //Straight line.
-                //Vertical
-                if(Mx === Rx)
-                    return pointOnVertical(x, y, minY + (gridSize * 3), maxY, Mx + gridSize, (gridSize / 4));
-                //Horizontal
-                else return pointOnHorizontal(x, y, minX + (gridSize * 2), maxX, My + (gridSize * 2 - (gridSize / 2)), (gridSize / 4));
-            case 1: //Segmented line - vertical junction.
+            // case 0: //Straight line.
+            //     //Vertical
+            //     if(Mx === Rx)
+            //         return pointOnVertical(x, y, minY + (gridSize * 3), maxY, Mx + gridSize, (gridSize / 4));
+            //     //Horizontal
+            //     else return pointOnHorizontal(x, y, minX + (gridSize * 2), maxX, My + (gridSize * 2 - (gridSize / 2)), (gridSize / 4));
+            case "v": //Segmented line - vertical junction.
                 return pointOnHorizontal(x, y, minX + (gridSize * 2), minX + (maxX - minX) * this.junction + gridSize, (minXIsM ? My: Ry) + (gridSize * 2 - (gridSize / 2)) , (gridSize / 4))
                     || pointOnVertical(x, y, minY + (gridSize * 2 - (gridSize / 2)), maxY + (gridSize * 2 - (gridSize / 2)), minX + (maxX - minX) * this.junction + gridSize, (gridSize / 4))
                     || pointOnHorizontal(x, y, minX + (maxX - minX) * this.junction + gridSize, maxX, (minXIsM ? Ry  : My) + (gridSize * 2 - (gridSize / 2)), (gridSize / 4));
-            case 2: //Segmented line - horizontal junction.
+            case "h": //Segmented line - horizontal junction.
                 return pointOnVertical(x, y, minY + (gridSize * 3), minY + (maxY - minY) * this.junction + (gridSize * 2 - (gridSize / 2)), (minYIsM ? Mx : Rx) + gridSize, (gridSize / 4))
                     || pointOnHorizontal(x, y, minX + gridSize, maxX + gridSize, minY + (maxY - minY) * this.junction + (gridSize * 2 - (gridSize / 2)), (gridSize / 4))
                     || pointOnVertical(x, y, minY + (maxY - minY) * this.junction + (gridSize * 2 - (gridSize / 2)), maxY, (minYIsM ? Rx : Mx) + gridSize, (gridSize / 4));
@@ -118,8 +130,4 @@ class RelationToR
         return false;
     }
 
-    update()
-    {
-
-    }
 }
