d3a7d29eebc99a3f9f1f1ba3fbb269a59df3c408
MINH KHANG TRAN <ktran2@oswego.edu>
2022-05-03 16:58:21 -0400

    Add email authentication config

--- config.py
+++ config.py
@@ -10,8 +10,10 @@ load_dotenv(path.join(basedir, '.env'))
 class Config:
     """Set Flask configuration from environment variables."""
 
+    # main config
     FLASK_ENV = os.getenv('FLASK_ENV')
     SECRET_KEY = os.getenv('SECRET_KEY') or os.urandom(24)
+    SECURITY_PASSWORD_SALT = os.getenv('SECURITY_PASSWORD_SALT')
 
     # Google Authentication
     GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
@@ -20,3 +22,16 @@ class Config:
 
     # SQL
     DATABASE_URL = 'sqlite:///db.sqlite'
+
+    # mail settings
+    MAIL_SERVER = os.getenv('APP_MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.getenv('APP_MAIL_PORT', 465))
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+
+    # gmail authentication
+    MAIL_USERNAME = os.environ['APP_MAIL_USERNAME']
+    MAIL_PASSWORD = os.environ['APP_MAIL_PASSWORD']
+
+    # mail accounts
+    MAIL_DEFAULT_SENDER = 'from@example.com'
