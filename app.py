3e66b671fa941a0585693b8358cc8a14c1bdbd71
MINH KHANG TRAN <ktran2@oswego.edu>
2022-05-03 17:00:22 -0400

    Update app.py for deployment

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from modules import create_app
+import os
 
 if __name__ == '__main__':
     app = create_app()
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = "1"
+    # app.run(debug=True, host="0.0.0.0", port=5000, threaded=True)
     app.run(debug=True, ssl_context="adhoc", threaded=True)
