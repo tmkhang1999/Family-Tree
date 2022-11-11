ec017047bf34f84ac39af3de40c4d9666dae3355
MINH KHANG TRAN <74235084+tmkhang1999@users.noreply.github.com>
2022-11-11 17:46:10 -0500

    Add gunicorn_app in main

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ from modules import create_app
 from utils.config import config
 from utils import database
 
-if __name__ == '__main__':
-    # Set up database
-    database.Database().setup()
+# Set up database
+database.Database().setup()
 
+if __name__ == '__main__':
     # Initialize app
     app = create_app()
 
     # Running
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = "1"
     host, port = config['app']['host'], config['app']['port']
     app.run(debug=True, host=host, port=port, threaded=True)
+else:
+    gunicorn_app = create_app()
